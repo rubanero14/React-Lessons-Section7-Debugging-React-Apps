04cd35f51ccecd2f118f6b35fe2b2dd15aec8510
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-18 23:16:45 +0800

    deploy

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
-
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-import './App.css';
+import React, { useState } from "react";
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+import "./App.css";
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
   ]);
 
   let countIds = courseGoals.length;
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
-      console.log(countIds, typeof countIds)
+      console.log(countIds, typeof countIds);
       updatedGoals.unshift({ text: enteredText, id: `g${++countIds}` });
       return updatedGoals;
     });
   };
 
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
       return updatedGoals;
     });
   };
 
   let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
   );
 
   if (courseGoals.length > 0) {
@@ -39,14 +38,12 @@ const App = () => {
   }
 
   return (
-      <div>
-        <section id="goal-form">
-          <CourseInput onAddGoal={addGoalHandler} />
-        </section>
-        <section id="goals">
-          {content}
-        </section>
-      </div>
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">{content}</section>
+    </div>
   );
 };
 
--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import styles from './CourseInput.module.css';
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
+import buttonStyle from "../../UI/Button/Button.module.css";
 
 const CourseInput = (props) => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
@@ -21,18 +22,30 @@ const CourseInput = (props) => {
       return;
     }
     props.onAddGoal(enteredValue);
-    setEnteredValue('');
+    setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div
-        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
       >
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
+        <label>Goals</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
+      <a
+        className={buttonStyle.button}
+        href="https://github.com/rubanero14/React-Lessons-Section7-Debugging-React-Apps"
+        target="_blank"
+        style={{ marginLeft: "5px" }}
+      >
+        Source Code
+      </a>
     </form>
   );
 };
